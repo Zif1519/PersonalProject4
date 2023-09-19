c7af95fa1806860d42059f1a45e89ff5d796e1a0
Zif1519 <141081153+Zif1519@users.noreply.github.com>
2023-09-19 18:52:41 +0900

    [UPDATE] Added Events, CallEvent Method
    Added 3 events. For status,inventory,viewstate changes

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum VIEWSTATE { MAIN = 0, STATUS, INVENTORY }
 public class UI_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UI_Manager Instance;
+
+    public VIEWSTATE _CurrentViewState = VIEWSTATE.MAIN;
+    public event Action<StatusData> OnStatusChanged;
+    public event Action<InventoryData> OnInventoryDataChanged;
+    public event Action<VIEWSTATE> OnViewStateChanged;
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void CallEventStatusChanged(StatusData newStatusData)
     {
-        
+        OnStatusChanged?.Invoke(newStatusData);
     }
+    public void CallEventInventoryChanged(InventoryData newInventoryData)
+    {
+        OnInventoryDataChanged?.Invoke(newInventoryData);
+    }
+    
+    public void CallEventViewStateChanged(VIEWSTATE newViewState)
+    {
+        OnViewStateChanged?.Invoke(newViewState);
+    }
+}
+
+public class StatusData
+{
+    [SerializeField] public int _Attack;
+    [SerializeField] public int _Defence;
+    [SerializeField] public int _Health;
+    [SerializeField] public int _Critical;
+    [SerializeField] public int _Dodge;
+    [SerializeField] public int _Speed;
+}
+
+public class InventoryData
+{
+
 }
